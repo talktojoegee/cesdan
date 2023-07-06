4423830d6d4355a53d0b3e72a59b44b014cc4048
talktojoegee <talktojoegee@gmail.com>
2023-07-06 17:20:01 +0100

    mobile logo changed

--- resources/views/partials/_app-header.blade.php
+++ resources/views/partials/_app-header.blade.php
@@ -1,9 +1,9 @@
 <div class="app-header header">
     <div class="container-fluid">
         <div class="d-flex">
-            <a class="header-brand d-md-none" href="index.html">
-                <img src="/assets/images/brand/main.png" class="header-brand-img mobile-icon" alt="logo">
-                <img src="/assets/images/brand/main.png" class="header-brand-img desktop-logo mobile-logo" alt="logo">
+            <a class="header-brand d-md-none" href="#">
+                <img src="/assets/images/brand/cidsan-logo.png" class="header-brand-img mobile-icon" alt="logo">
+                <img src="/assets/images/brand/cidsan-logo.png" class="header-brand-img desktop-logo mobile-logo" alt="logo">
             </a>
             <a aria-label="Hide Sidebar" class="app-sidebar__toggle" data-toggle="sidebar" href="#">
                 <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M21 11.01L3 11v2h18zM3 16h12v2H3zM21 6H3v2.01L21 8z"/></svg>
