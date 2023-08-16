3827a6c3a528f193bc765d17412e1637c36eb61a
talktojoegee <talktojoegee@gmail.com>
2023-08-16 11:26:30 +0100

    Profile update should lead to pending approval

--- app/Models/User.php
+++ app/Models/User.php
@@ -234,7 +234,7 @@ class User extends Authenticatable implements JWTSubject
         $user->referee_membership_no = $request->refereeOneMembershipNo ?? null;
         $user->referee_mobile_no = $request->refereeOneGSM ?? null;
         $user->sponsoring_district = $request->sponsoringDistrictSociety ?? null;
-        $user->account_status = 1;
+        $user->account_status = 2;
         $user->save();
         return $user;
     }
