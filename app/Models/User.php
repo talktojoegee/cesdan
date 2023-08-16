dfd53c54542800e4f9c74d8a0acd47078995d643
talktojoegee <talktojoegee@gmail.com>
2023-08-16 11:56:29 +0100

    online payment

--- app/Models/User.php
+++ app/Models/User.php
@@ -150,7 +150,7 @@ class User extends Authenticatable implements JWTSubject
         $user->payment_method = $method ?? 0;
         $user->payment_method_verification = $verify ?? 0;
         $user->tenant_id = 1;
-        $user->account_status = $method == 1 ? 2 : 1;
+        $user->account_status = $method == 1 ? 0 : 1;
         $user->active_sub_key = $registrationNo ?? null; //active_sub_key holds the registration number
         $user->slug = Str::slug($surname).'-'.substr(sha1(time()),32,40);
         $user->save();
